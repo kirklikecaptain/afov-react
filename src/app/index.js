e0405b549d3e2f6142aacc44f779dcb6fc1fcc54
Kirk Hodgman <kjhodgman@gmail.com>
2019-02-06 20:13:50 -0800

    fuu

--- src/app/index.js
+++ src/app/index.js
@@ -26,27 +26,7 @@ function App(props) {
       <Head>
         <title>{props.title}</title>
 				<meta name="google-site-verification" content="N-gAFfxmn6FI9m99GPh7jUF2XA3aDpfVAhNlbdWxdJU" />
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `
-							(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-							new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-							j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-							'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-							})(window,document,'script','dataLayer','GTM-K7XWHDW');
-						`
-					}}
-				/>
       </Head>
-			<noscript>
-				<iframe
-					title="google-tag-manager"
-					src="https://www.googletagmanager.com/ns.html?id=GTM-K7XWHDW"
-					height="0"
-					width="0"
-					style={{ display: "none", visibility: "hidden" }}
-				/>
-			</noscript> */}
 			<StyledContainer>
 				<Nav />
 				<div className='main-content'>
