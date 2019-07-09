2b58bdf1877d65d8f9a8925179ba92934e6d2947
Kirk Hodgman <kjhodgman@gmail.com>
2019-07-08 22:09:37 -0700

    safari fix

--- src/app/components/HomeHero.js
+++ src/app/components/HomeHero.js
@@ -107,6 +107,7 @@ const StyledHero = styled.div`
       }
     }
   }
+
   .mobile-overlay {
     position: absolute;
     top: 0;
@@ -209,6 +210,7 @@ const StyledHero = styled.div`
           : lighten(0.3, '#333')};
     }
   }
+
   .overlay {
     z-index: 1000;
     background: ${props =>
@@ -267,6 +269,15 @@ const StyledHero = styled.div`
       .full-button {
         font-size: 1.2rem;
       }
+    }
+  }
+
+  @media screen and (min-color-index: 0) and (-webkit-min-device-pixel-ratio: 0) {
+    .overlay {
+      display: none;
+    }
+    .mobile-overlay {
+      background: none;
     }
   }
 `;
